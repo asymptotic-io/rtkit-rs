53a6fce118f7b05181ea4290016bd985e3961ba9
Sanchayan Maity <me@sanchayanmaity.net>
2025-06-13 17:01:20 +0530

    Add thread priority and real time methods

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ use zbus::zvariant::Value;
 
 const RTKIT_OBJECT_PATH: &str = "/org/freedesktop/RealtimeKit1";
 const RTKIT_SERVICE_NAME: &str = "org.freedesktop.RealtimeKit1";
+const RTKIT_INTERFACE: &str = "org.freedesktop.RealtimeKit1";
 
 fn is_rtkit_available(connection: &Connection) -> Result<bool> {
     let message = connection.call_method(
@@ -95,6 +96,72 @@ impl RTKit {
             Err(e) => Err(e.into()),
         }
     }
+
+    pub fn make_thread_high_priority(&self, thread_id: u64, priority: i32) -> anyhow::Result<()> {
+        self.connection.call_method(
+            Some(RTKIT_SERVICE_NAME),
+            RTKIT_OBJECT_PATH,
+            Some(RTKIT_INTERFACE),
+            "MakeThreadHighPriority",
+            &(thread_id, priority),
+        )?;
+
+        Ok(())
+    }
+
+    pub fn make_thread_high_priority_with_pid(
+        &self,
+        process_id: u64,
+        thread_id: u64,
+        priority: i32,
+    ) -> anyhow::Result<()> {
+        self.connection.call_method(
+            Some(RTKIT_SERVICE_NAME),
+            RTKIT_OBJECT_PATH,
+            Some(RTKIT_INTERFACE),
+            "MakeThreadHighPriorityWithPID",
+            &(process_id, thread_id, priority),
+        )?;
+
+        Ok(())
+    }
+
+    pub fn make_thread_realtime(&self, thread_id: u64, priority: u32) -> anyhow::Result<()> {
+        self.connection.call_method(
+            Some(RTKIT_SERVICE_NAME),
+            RTKIT_OBJECT_PATH,
+            Some(RTKIT_INTERFACE),
+            "MakeThreadRealtime",
+            &(thread_id, priority),
+        )?;
+
+        Ok(())
+    }
+
+    pub fn make_thread_realtime_with_pid(
+        &self,
+        process_id: u64,
+        thread_id: u64,
+        priority: u32,
+    ) -> anyhow::Result<()> {
+        self.connection.call_method(
+            Some(RTKIT_SERVICE_NAME),
+            RTKIT_OBJECT_PATH,
+            Some(RTKIT_INTERFACE),
+            "MakeThreadRealtimeWithPID",
+            &(process_id, thread_id, priority),
+        )?;
+
+        Ok(())
+    }
+
+    pub fn current_thread_id() -> u64 {
+        unsafe { libc::syscall(libc::SYS_gettid) as u64 }
+    }
+
+    pub fn current_process_id() -> u64 {
+        std::process::id() as u64
+    }
 }
 
 #[cfg(test)]
@@ -111,5 +178,40 @@ mod tests {
         assert_eq!(rtkit.max_realtime_priority().unwrap(), 20);
         assert_eq!(rtkit.min_nice_level().unwrap(), -15);
         assert_eq!(rtkit.rttime_usec_max().unwrap(), 200000);
+    }
+
+    #[test]
+    fn test_thread_id_retrieval() {
+        assert!(RTKit::current_thread_id() > 0);
+    }
+
+    #[test]
+    fn test_process_id_retrieval() {
+        assert!(RTKit::current_process_id() > 0);
+    }
+
+    #[test]
+    fn test_make_thread_high_priority() {
+        let rtkit = RTKit::new();
+        assert!(rtkit.is_ok());
+        let rtkit = rtkit.unwrap();
+
+        let thread_id = RTKit::current_thread_id();
+        assert!(rtkit.make_thread_high_priority(thread_id, -10).is_ok());
+    }
+
+    #[test]
+    fn test_make_thread_high_priority_with_pid() {
+        let rtkit = RTKit::new();
+        assert!(rtkit.is_ok());
+        let rtkit = rtkit.unwrap();
+
+        let pid = RTKit::current_process_id();
+        let thread_id = RTKit::current_thread_id();
+        assert!(
+            rtkit
+                .make_thread_high_priority_with_pid(pid, thread_id, -10)
+                .is_ok()
+        )
     }
 }
